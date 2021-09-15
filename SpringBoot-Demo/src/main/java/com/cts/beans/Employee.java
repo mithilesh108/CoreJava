2ef39c92ef810dce9a4d326efbd84bac990dcb8b
unknown <tech.mithilesh108@gmail.com>
2021-09-15 12:10:24 +0530

    spring booot appliation

---
+++ SpringBoot-Demo/src/main/java/com/cts/beans/Employee.java
@@ -0,0 +1,9 @@
+package com.cts.beans;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private int age;
+	
+}
---
+++ SpringBoot-Demo/src/test/java/com/cts/java/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
